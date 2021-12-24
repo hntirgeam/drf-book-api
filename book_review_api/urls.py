9a4ae23ab29e429c363e437b0e558adda051e766
hntirgeam <kmacroviperaz@gmail.com>
2021-12-25 00:08:50 +0300

    urls

--- book_review_api/urls.py
+++ book_review_api/urls.py
@@ -1,13 +1,20 @@
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 from book_review_app import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
+
+router.register(r'api/v1/register', views.CreateAuthorView)
+
+router.register(r'api/authors', views.AuthorViewSet) 
+router.register(r'api/books', views.BookViewSet)
+router.register(r'api/genres', views.GenreViewSet)
+
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
+    path('api/v1/api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('api/v1/api-token-deauth/', views.LogoutView.as_view(), name='api_token_deauth')
+] + router.urls
