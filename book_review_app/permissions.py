bba447377d55ca37144338c3d13a637ce8501975
hntirgeam <kmacroviperaz@gmail.com>
2021-12-25 13:28:15 +0300

    more permissions

--- book_review_app/permissions.py
+++ book_review_app/permissions.py
@@ -1,8 +1,24 @@
 from rest_framework import permissions
 
 
+class IsOwnProfileOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj == request.user
+
 class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.author == request.user
+    
+class IsAdminOrReadOnly(permissions.BasePermission): 
+    def has_permission(self, request, view):
+        print(request.user, "==", request.user.is_staff)
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return bool(request.user and request.user.is_staff)
+    
+    
+    
