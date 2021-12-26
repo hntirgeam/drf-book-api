9385a705cc3ff4374cb4863d58526e9645766ccc
hntirgeam <kmacroviperaz@gmail.com>
2021-12-26 18:37:42 +0300

    removed print

--- book_review_app/permissions.py
+++ book_review_app/permissions.py
@@ -17,7 +17,6 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        print(request.user, "==", request.user.is_staff)
         if request.method in permissions.SAFE_METHODS:
             return True
         return bool(request.user and request.user.is_staff)
